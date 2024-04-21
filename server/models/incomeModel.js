10016ab2ba8e51fb6fdf6cb0107844a4243f4a44
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-04-21 14:23:26 +0700

    21/04/67

--- server/models/incomeModel.js
+++ server/models/incomeModel.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize")
 module.exports = (sequelize,Sequelize) => {
     const Income = sequelize.define('Incomes', {
         incomeDetails: DataTypes.STRING,
-        amount: DataTypes.DECIMAL,
+        amount: DataTypes.INTEGER,
         incomeDate: DataTypes.DATE
     })
     return Income
